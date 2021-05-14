69b8b76f2a397ae4c0941a3bcdd3976aba6e5edf
Anthony <anthony.zink@outlook.fr>
2021-05-14 19:46:26 +0200

    fix seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,30 @@
-import  { PrismaClient } from '@prisma/client';
-import { hashPassword } from '../src/utils/auth';
+import { PrismaClient } from "@prisma/client";
+import { hashPassword } from "../src/utils/auth";
 const prisma = new PrismaClient();
 
 async function main() {
-
-    await prisma.users.create({
-        data:{
+    const user = await prisma.user.create({
+        data: {
             email: "admin@admin.fr",
-            password: await hashPassword('admin')
-        }
-    })
+            password: await hashPassword("admin"),
+            username: "Admin",
+            twitterId: "Admin1",
+        },
+    });
+
+    await prisma.tweet.create({
+        data: {
+            content: "Je suis un tweet du seed",
+            userId: user.id,
+        },
+    });
 }
 
 main()
-  .catch(e => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
